082bc2a256cc9a1b28efb1350acfa0e616c1cf81
Hong Xu <hong@topbug.net>
2015-06-25 21:44:18 -0700

    Add my name to the PDF document (author)

--- doc/conf.py
+++ doc/conf.py
@@ -234,7 +234,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'ParamComparison.tex', 'ParamComparison Documentation',
-   '\\textless{}hong@topbug.net\\textgreater{}', 'manual'),
+   'Hong Xu \\textless{}hong@topbug.net\\textgreater{}', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
